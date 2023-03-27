5ef99cba2e2b774aea5146f877820aa8819fe6f0
snow-sr <joaofelipi877@gmail.com>
2023-03-27 19:56:17 -0300

    [fix] Fixed reading rfid problems

---
+++ Slave.cpp
@@ -0,0 +1,38 @@
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN 22 // Configurable, see typical pin layout above
+#define SS_PIN 21  // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+
+void setup()
+{
+  Serial.begin(115200); // Initialize serial communications with the PC
+  while (!Serial)
+    ;                                // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();                       // Init SPI bus
+  mfrc522.PCD_Init();                // Init MFRC522
+  delay(4);                          // Optional delay. Some board do need more time after init to be ready, see Readme
+  mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+}
+
+void loop()
+{
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+
+  // Select one of the cards
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+}
--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,30 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char *ssid = "SNOW";
-const char *password = "poopfart";
+const char *ssid = "ifc_wifi";
+const char *password = "";
 
-String ApiUrl = "https://doorauthmock-production.up.railway.app/";
+String ApiUrl = "http://191.52.58.120:8087";
 
 #define SS_PIN 21
 #define RST_PIN 22
 #define CARD_READ_DELAY 2000 // Tempo em milissegundos para esperar antes de ler outro cartão
-// #define RELAY_PIN 7
-#define DEBOUNCE_DELAY 50 // Tempo em milissegundos para esperar antes de mudar o estado do relé novamente
+#define RELAY_PIN 13         // Relay at pin 13
+#define DEBOUNCE_DELAY 50    // Tempo em milissegundos para esperar antes de mudar o estado do relé novamente
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 unsigned long lastCardReadTime = 0;
 bool isRelayOn = false;
 
 /* Funçao para verificar se a Api está no ar*/
-bool checkApi()
+bool check_api()
 {
   HTTPClient http;
   http.begin(ApiUrl);
   int httpCode = http.GET();
   if (httpCode > 0)
   {
-    Serial.printf("[HTTP] GET... code: %d n", httpCode);
+    Serial.printf("[HTTP] GET... code: %d", httpCode);
     if (httpCode == HTTP_CODE_OK)
     {
       String payload = http.getString();
@@ -43,10 +43,63 @@ bool checkApi()
   return true;
 }
 
+bool auth_rfid(String rfid)
+{
+  /*
+   * Receive the rfid from the reader, and then, make a post to apiUrl/door/
+   * Passing the rfid on the body of the request, as a json
+   * If the rfid is valid, the api will return a 200 status code
+   * If the rfid is invalid, the api will return a 401 status code
+   * If the api is down, the api will return a 500 status code
+   */
+
+  HTTPClient http;
+  http.begin(ApiUrl + "/door/"); // Specify destination for HTTP request
+  http.addHeader("Content-Type", "application/json");
+  String json = "{\"rfid\": \"" + rfid + "\"}";
+  int httpCode = http.POST(json); // Send the request
+
+  if (httpCode > 0)
+  {
+    Serial.printf("[HTTP] POST... code: %d", httpCode);
+    if (httpCode == HTTP_CODE_OK)
+    {
+      String payload = http.getString();
+      Serial.println(payload);
+      return true;
+    }
+    if (httpCode == HTTP_CODE_UNAUTHORIZED)
+    {
+      Serial.println("Cartao nao autorizado");
+      return false;
+    }
+  }
+  else
+  {
+    Serial.printf("[HTTP] POST... failed, error: %s n", http.errorToString(httpCode).c_str());
+    return false;
+  }
+
+  http.end();
+}
+
+void unlock_door()
+{
+  digitalWrite(RELAY_PIN, HIGH);
+  Serial.println("ABRINDO");
+  isRelayOn = true;
+  delay(DEBOUNCE_DELAY);
+  digitalWrite(RELAY_PIN, LOW);
+  Serial.println("FECHANDO");
+  isRelayOn = false;
+}
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Iniciando...");
+
+  pinMode(RELAY_PIN, OUTPUT);
 
   WiFi.mode(WIFI_STA); // Optional
   WiFi.begin(ssid, password);
@@ -62,71 +115,62 @@ void setup()
   Serial.println("\nConnected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.println(WiFi.subnetMask());
+  Serial.println(WiFi.gatewayIP());
+  Serial.println(WiFi.dnsIP());
 
   SPI.begin();
   rfid.PCD_Init();
-  checkApi();
+  Serial.println("Leitor RFID inicializado, aguardando cartoes...");
+  check_api();
   // pinMode(RELAY_PIN, OUTPUT);
 }
 
 void loop()
 {
   // Verifica se há um novo cartão presente ou se o cartão atual foi lido novamente
-  if (rfid.PICC_IsNewCardPresent() || rfid.PICC_ReadCardSerial())
+
+  // if (!rfid.PICC_IsNewCardPresent())
+  // {
+  //   return;
+  // }
+
+  // if (!rfid.PICC_ReadCardSerial())
+  // {
+  //   return;
+  // }
+
+  if(rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
   {
-    // Verifica se já passou tempo suficiente desde a última leitura
-    if (millis() - lastCardReadTime > CARD_READ_DELAY)
+
+  // Verifica se já passou tempo suficiente desde a última leitura
+  if (millis() - lastCardReadTime > CARD_READ_DELAY)
+  {
+    // Lê o conteúdo do cartão e o imprime no console
+    String conteudo = "";
+    for (byte i = 0; i < rfid.uid.size; i++)
     {
-      // Lê o conteúdo do cartão e o imprime no console
-      String conteudo = "";
-      for (byte i = 0; i < rfid.uid.size; i++)
-      {
-        conteudo.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
-        conteudo.concat(String(rfid.uid.uidByte[i], HEX));
-      }
-      conteudo.toUpperCase();
-      Serial.println("Conteúdo do cartão: " + conteudo);
-
-      // Liga o relé e aguarda um tempo antes de desligá-lo novamente
-      // digitalWrite(RELAY_PIN, HIGH);
-      Serial.println("ABRINDO");
-      // isRelayOn = true;
-      delay(DEBOUNCE_DELAY);
-      // digitalWrite(RELAY_PIN, LOW);
-      Serial.println("FECHANDO");
-      // isRelayOn = false;
-
-      // Reinicia o leitor RFID para procurar novos cartões
-      rfid.PCD_Init();
-
-      // Registra a hora da última leitura
-      lastCardReadTime = millis();
+      conteudo.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
+      conteudo.concat(String(rfid.uid.uidByte[i], HEX));
     }
+    conteudo.toUpperCase();
+    Serial.println("Card content: " + conteudo);
+    // if conteudo is not empty, then send to the api
+    if (conteudo != "")
+    {
+      if (auth_rfid(conteudo))
+      {
+        unlock_door();
+      }
+      else
+      {
+        Serial.println("Porta nao desbloqueada");
+      }
+    }
+    return;
   }
-}
 
-/* Função para ler o cartão RFID e retornar o conteúdo como uma string.
- * Retorna uma string vazia se não houver cartão presente ou se ocorrer um erro na leitura.
- */
-String leCartaoRFID()
-{
-  if (!rfid.PICC_IsNewCardPresent())
-  {
-    return "";
+    // Registra a hora da última leitura
+    lastCardReadTime = millis();
   }
-
-  if (!rfid.PICC_ReadCardSerial())
-  {
-    return "";
-  }
-
-  String conteudo = "";
-  for (byte i = 0; i < rfid.uid.size; i++)
-  {
-    conteudo.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""));
-    conteudo.concat(String(rfid.uid.uidByte[i], HEX));
-  }
-  conteudo.toUpperCase();
-
-  return conteudo;
 }
