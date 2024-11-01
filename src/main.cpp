ad6ab7eca5b4bec1cd71105cea6fd53edd9e6096
jonatassilvaperaza@gmail.com <jonatassilvaperaza@gmail.com>
2024-11-01 16:53:11 -0300

    Remover verificação de API no loop principal e simplificar resposta do servidor para abertura de porta

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <SPI.h>
 #include "FS.h"
 #include <WebServer.h>
-#define ETH_CS 5         // Defina o pino CS usado pelo ENC28J60
+#define ETH_CS 5   
+      // Defina o pino CS usado pelo ENC28J60
+WebServer server(19003);
 
 byte mac[] = {0x7a, 0x12, 0x58, 0x5e, 0x6c, 0x33}; // Defina seu endereço MAC aqui
 
@@ -22,45 +24,15 @@ void setup()
   // Exibe o IP obtido via DHCP
   Serial.print("Conectado com IP: ");
   Serial.println(Ethernet.localIP());
-  WebServer server(19003);
 server.on("/open-door", HTTP_GET, []()
           {
-    if (server.hasHeader("Authorization") && server.header("Authorization") == "Bearer " + BEARER_TOKEN) {
-      unlock_door();
-      server.send(200, "application/json", "{\"success\": true, \"message\": \"Porta aberta com sucesso\"}");
-    } else {
-      server.send(401, "application/json", "{\"success\": false, \"message\": \"Token Bearer inválido\"}");
-    } });
+            server.send(200, "text/plain", "Porta aberta!");
+          });
 
 server.begin();
 }
 
 void loop()
 {
-  if (Ethernet.localIP() != IPAddress(0, 0, 0, 0))
-  {
-    HTTPClient http;
-    http.begin("http://www.google.com");
-    int httpCode = http.GET();
 
-    if (httpCode > 0)
-    {
-      Serial.printf("Código HTTP: %d\n", httpCode);
-      String payload = http.getString();
-      Serial.println("Resposta:");
-      Serial.println(payload);
-    }
-    else
-    {
-      Serial.printf("Erro na requisição: %s\n", http.errorToString(httpCode).c_str());
-    }
-
-    http.end();
-  }
-  else
-  {
-    Serial.println("Conexão de rede não estabelecida.");
-  }
-
-  delay(10000);
 }
