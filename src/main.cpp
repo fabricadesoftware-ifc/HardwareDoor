7e20643229be41271a76cc99fe41e7c81b0c09db
jonatassilvaperaza@gmail.com <jonatassilvaperaza@gmail.com>
2024-11-19 13:27:32 -0300

    Adicionar suporte a cache de RFID e atualizar configurações do projeto

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,32 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <WebServer.h>
+#include <vector>
+#include <map>
+#include <ArduinoJson.h>
 
 const char *ssid = "ifc_wifi";
 const char *password = "";
 const int TickerTimer = 500;
+const int TickerTimer2 = 600;
 String ApiUrl = "https://door-api.fabricadesoftware.ifc.edu.br/api";
 Ticker ticker;
 
 const String BEARER_TOKEN = "fabdor-dPluQTwdJJ4tamtnP0i7J34UqphHuJTdUugKt2YMJgQeoAS5qs1fFi4My";
 #define LED_BUILTIN 2
 
-#define SS_PIN 21    // Pino SS para o leitor RFID
-#define RST_PIN 22   // Pino RST para o leitor RFID
-#define RELAY_PIN 13 // Pino para o relé (controle da porta)
+#define SS_PIN 21     // Pino SS para o leitor RFID
+#define RST_PIN 22    // Pino RST para o leitor RFID
+#define RELAY_PIN 13  // Pino para o relé (controle da porta)
 #define BUZZER_PIN 12 // Pino para o buzzer
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 WebServer server(19003);
 
-bool cadastroAtivo = false; 
+bool cadastroAtivo = false;
+
+// Cache de tags autorizadas
+std::map<String, std::pair<bool, unsigned long>> rfidCache;
 
 void logEvent(String type, String message)
 {
@@ -32,7 +39,7 @@ void logEvent(String type, String message)
   String json = "{\"type\": \"" + type + "\", \"message\": \"" + message + "\"}";
   http.begin(ApiUrl + "/logs");
   http.addHeader("Content-Type", "application/json");
-  int httpCode = http.POST(json);
+  http.POST(json);
   http.end();
 }
 
@@ -42,7 +49,53 @@ void imAlive()
   String json = "{\"ip\": \"" + WiFi.localIP().toString() + "\"}";
   http.begin(ApiUrl + "/health/ip");
   http.addHeader("Content-Type", "application/json");
-  int httpCode = http.POST(json);
+  http.POST(json);
+  http.end();
+}
+
+void atualizarCache()
+{
+  HTTPClient http;
+  http.begin(ApiUrl + "/tags/");
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("Authorization", "Bearer " + BEARER_TOKEN);
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK)
+  {
+    String payload = http.getString();
+    logEvent("INFO", "Resposta da API recebida: " + payload);
+
+    DynamicJsonDocument doc(4096);
+    DeserializationError error = deserializeJson(doc, payload);
+
+    if (error)
+    {
+      logEvent("ERROR", "Erro ao analisar JSON: " + String(error.c_str()));
+      return;
+    }
+
+    JsonArray tags = doc["data"].as<JsonArray>();
+    rfidCache.clear(); // Limpa o cache antes de atualizar
+
+    for (JsonObject tag : tags)
+    {
+      String rfid = tag["rfid"].as<String>();
+      bool isValid = tag["valid"].as<bool>();
+
+      if (isValid)
+      {
+        rfidCache[rfid] = std::make_pair(true, millis());
+      }
+    }
+
+    logEvent("INFO", "Cache atualizado com sucesso com " + String(rfidCache.size()) + " tags válidas.");
+  }
+  else
+  {
+    logEvent("ERROR", "Falha ao conectar com a API: " + http.errorToString(httpCode));
+  }
+
   http.end();
 }
 
@@ -50,50 +103,36 @@ void unlock_door()
 {
   digitalWrite(RELAY_PIN, HIGH);
   delay(50);
-  digitalWrite(RELAY_PIN, LOW); 
+  digitalWrite(RELAY_PIN, LOW);
 }
 
-// Função para verificar e autenticar RFID na API
 bool auth_rfid(String rfidCode)
 {
-  HTTPClient http;
-  http.begin(ApiUrl + "/tags/door");
-  http.addHeader("Content-Type", "application/json");
-  http.addHeader("Authorization", "Bearer " + BEARER_TOKEN);
-  http.setTimeout(5000);
-  String json = "{\"rfid\": \"" + rfidCode + "\"}";
-  int httpCode = http.POST(json);
-
-
-  if (httpCode == HTTP_CODE_OK)
+  if (rfidCache.find(rfidCode) != rfidCache.end() && rfidCache[rfidCode].first)
   {
-    return true;
+    return true; // Tag encontrada no cache e válida
   }
-  else if (httpCode == HTTP_CODE_UNAUTHORIZED)
-  {
-    digitalWrite(BUZZER_PIN, HIGH);
-    delay(500);
-    digitalWrite(BUZZER_PIN, LOW);
-    logEvent("WARN", "RFID não autorizado " + rfidCode);
-    return false;
-  }
-  http.end();
+
+  // Caso contrário, a tag não é válida
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(500);
+  digitalWrite(BUZZER_PIN, LOW);
+  logEvent("WARN", "RFID não autorizado: " + rfidCode);
   return false;
 }
 
-// Função para cadastrar um novo RFID
 void cadastrar_rfid(String rfidCode)
 {
   HTTPClient http;
   logEvent("INFO", "Cadastrando RFID: " + rfidCode);
-  http.begin(ApiUrl + "/tags/" + rfidCode); // Define o endpoint da API
+  http.begin(ApiUrl + "/tags/" + rfidCode);
   http.addHeader("Content-Type", "application/json");
   http.addHeader("Authorization", "Bearer " + BEARER_TOKEN);
   int httpCode = http.POST("{}");
 
   if (httpCode == HTTP_CODE_CREATED)
   {
-    logEvent("SUCESS", "RFID cadastrado com sucesso");
+    logEvent("SUCCESS", "RFID cadastrado com sucesso");
   }
   else if (httpCode == HTTP_CODE_CONFLICT)
   {
@@ -101,11 +140,10 @@ void cadastrar_rfid(String rfidCode)
   }
   else
   {
-    logEvent("ERROR","Erro ao cadastrar RFID");
+    logEvent("ERROR", "Falha ao criar RFID: " + http.errorToString(httpCode));
   }
 }
 
-// Função para alternar entre modo cadastro e normal
 void alternarModoCadastro()
 {
   cadastroAtivo = !cadastroAtivo;
@@ -113,7 +151,6 @@ void alternarModoCadastro()
   digitalWrite(LED_BUILTIN, cadastroAtivo ? HIGH : LOW); // LED indica modo ativo
 }
 
-// Função para processar cartões RFID
 String processarCartao()
 {
   String rfidCode = "";
@@ -126,13 +163,13 @@ String processarCartao()
 
   if (cadastroAtivo)
   {
-    cadastrar_rfid(rfidCode); // Cadastra o RFID no modo de cadastro
+    cadastrar_rfid(rfidCode);
   }
   else
   {
     if (auth_rfid(rfidCode))
     {
-      unlock_door(); // Desbloqueia a porta se o RFID for autorizado
+      unlock_door();
     }
   }
   SPI.begin();
@@ -140,7 +177,6 @@ String processarCartao()
   return rfidCode;
 }
 
-// Função para processar leituras de RFID
 void verificarCartaoRFID()
 {
   if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
@@ -149,15 +185,13 @@ void verificarCartaoRFID()
     delay(100);
     digitalWrite(BUZZER_PIN, LOW);
     const String tag = processarCartao();
-    logEvent("INFO", "Cartão detectado. " + tag);
-    rfid.PICC_HaltA(); // Finaliza a leitura do cartão
+    logEvent("INFO", "Cartão detectado: " + tag);
+    rfid.PICC_HaltA();
   }
 }
 
-// Função para iniciar o servidor e rotas
 void iniciarServidor()
 {
-  // Rota para alternar modo cadastro/normal
   server.on("/toggle-mode", HTTP_GET, []()
             {
     if (server.hasHeader("Authorization") && server.header("Authorization") == "Bearer " + BEARER_TOKEN) {
@@ -167,7 +201,6 @@ void iniciarServidor()
       server.send(401, "application/json", "{\"success\": false, \"message\": \"Token Bearer inválido\"}");
     } });
 
-  // Rota para abrir a porta manualmente
   server.on("/open-door", HTTP_GET, []()
             {
     if (server.hasHeader("Authorization") && server.header("Authorization") == "Bearer " + BEARER_TOKEN) {
@@ -200,13 +233,18 @@ void setup()
 
   SPI.begin();
   rfid.PCD_Init();
+
+  // Atualiza o cache imediatamente
+  atualizarCache();
+
   ticker.attach(TickerTimer, imAlive);
+  ticker.attach(TickerTimer2, atualizarCache);
 
   iniciarServidor();
 }
 
 void loop()
 {
-  server.handleClient(); // Processa as requisições HTTP
-  verificarCartaoRFID(); // Verifica se há cartões RFID
+  server.handleClient();
+  verificarCartaoRFID();
 }
